fd8c62d2c09b31684267a3dafaad3ec4da906a65
Salarlodin60 <104074628+Salarlodin60@users.noreply.github.com>
2022-04-25 19:28:43 +0430

    Salarlodin1.py
    pkg update &&  pkg upgrade  
    pkg install python  
    pkg install git  
    pip install bs4  
    pip install requests  
    pip install mechanize  
    pip install futures
    rm -rf Lodin1
    git clone https://github.com/Salarlodin60/Lodin1.git
    ls
    cd Lodin1
    ls
    python Salarlodin1.py

---
+++ Salarlodin1.py
@@ -0,0 +1,9 @@
+print("      THANKS FOR USE ME 😍")
+
+print("\033[1;32m Creators Salar Lodin + Salar Lodin")
+
+print("\033[1;33m         version :- 2.1 ")
+
+import Salarlodin
+
+Salarlodin.main_apv()
